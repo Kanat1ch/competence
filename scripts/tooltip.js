dfbcecd3939b3c54b9d088c67332f7db3d117253
kanatich <ik@moderntoken.com>
2022-02-01 19:48:52 +0300

    Add tooltip

---
+++ scripts/tooltip.js
@@ -0,0 +1,12 @@
+const tooltips = document.querySelectorAll('.tooltip')
+
+if (tooltips) {
+  tooltips.forEach(tooltip => {
+    tippy(tooltip, {
+      content: tooltip.getAttribute('data-tooltip'),
+      arrow: true,
+      trigger: 'click'
+    });
+  })
+}
+
